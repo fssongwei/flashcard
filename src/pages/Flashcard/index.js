a983f6dbba0c276c233837d5ab5e3eeb7ff547a7
Wei Song <ws446@cornell.edu>
2020-12-27 20:11:49 +0800

    add review record and fix pagination bug

--- src/pages/Flashcard/index.js
+++ src/pages/Flashcard/index.js
@@ -14,10 +14,16 @@ import {
 } from "antd";
 import getColor from "../../utilities/getColor";
 import MarkdownIt from "markdown-it";
-import { LeftOutlined, EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import {
+  LeftOutlined,
+  EditOutlined,
+  DeleteOutlined,
+  CheckOutlined,
+} from "@ant-design/icons";
 import { useDispatch } from "react-redux";
 import { deleteFlashcard } from "../../actions";
 import "./style.css";
+import moment from "moment";
 
 const { Title } = Typography;
 const { Content } = Layout;
@@ -25,23 +31,63 @@ const { Panel } = Collapse;
 const mdParser = new MarkdownIt(/* Markdown-it options */);
 
 const Flashcard = (props) => {
-  const { id } = useParams();
   const history = useHistory();
-  const [flashcard, setFlashcard] = useState(null);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const fetchFlashcard = async () => {
-      try {
-        let flashcard = (await axios.get(`/api/flashcards/${id}`)).data;
-        setFlashcard(flashcard);
-      } catch (error) {
-        message.error("Something went wrong!");
-      }
-    };
+  const { id } = useParams();
+  const [flashcard, setFlashcard] = useState(null);
 
+  const fetchFlashcard = async () => {
+    try {
+      let flashcard = (await axios.get(`/api/flashcards/${id}`)).data;
+      setFlashcard(flashcard);
+    } catch (error) {
+      message.error("Something went wrong!");
+    }
+  };
+
+  useEffect(() => {
     fetchFlashcard();
   }, [id]);
+
+  // pagination
+  let cardList = props.location.cardList;
+  if (!cardList) {
+    cardList = JSON.parse(window.sessionStorage.getItem("cardList"));
+  } else {
+    window.sessionStorage.setItem("cardList", JSON.stringify(cardList));
+  }
+  let currentPage = cardList ? cardList.indexOf(id) + 1 : 0;
+
+  // review
+  const setReview = async () => {
+    try {
+      await axios.get(`/api/review/${id}`);
+      await fetchFlashcard();
+    } catch (error) {
+      message.error("Something went wrong!");
+    }
+  };
+
+  const undoReview = async () => {
+    try {
+      await axios.delete(`/api/review/${id}`);
+      await fetchFlashcard();
+    } catch (error) {
+      message.error("Something went wrong!");
+    }
+  };
+
+  let reviewed = false;
+  if (
+    flashcard &&
+    flashcard.reviewRecord &&
+    flashcard.reviewRecord[0] &&
+    moment(flashcard.reviewRecord[0]).format("YYYYMMDD") ===
+      moment().format("YYYYMMDD")
+  ) {
+    reviewed = true;
+  }
 
   if (flashcard === null) {
     return (
@@ -51,15 +97,13 @@ const Flashcard = (props) => {
     );
   }
 
-  let cardList = props.location.cardList;
-  let currentPage = cardList ? cardList.indexOf(id) + 1 : 0;
-
   return (
     <Content
       style={{ width: "90%", maxWidth: "800px", margin: "50px auto" }}
       className="d-flex flex-column"
     >
       <div>
+        {/* Category and Button Group */}
         <div className="pb-4 d-flex justify-content-between">
           <div className="d-flex ">
             <Button
@@ -70,14 +114,23 @@ const Flashcard = (props) => {
             <h2 className="ps-2">{flashcard.category}</h2>
           </div>
           <div>
-            <Link to={`/edit/${id}`}>
-              <Button
-                shape="circle"
-                icon={<EditOutlined />}
-                type="primary"
-                className="me-2"
-              />
-            </Link>
+            <Button
+              shape={reviewed ? "round" : "circle"}
+              icon={<CheckOutlined />}
+              type={reviewed ? "primary" : "default"}
+              className="me-2"
+              onClick={reviewed ? undoReview : setReview}
+            >
+              {reviewed ? "Reviewed" : ""}
+            </Button>
+
+            <Button
+              shape="circle"
+              icon={<EditOutlined />}
+              type="primary"
+              className="me-2"
+              onClick={() => history.push(`/edit/${id}`)}
+            />
 
             <Popconfirm
               title="Are you sure to delete this flashcard?"
@@ -92,17 +145,21 @@ const Flashcard = (props) => {
           </div>
         </div>
 
-        <Title level={3}>{flashcard.title}</Title>
-        <div className="pb-5">
-          {flashcard.tags.map((tag) => {
-            return (
-              <Tag color={getColor(tag)} key={tag} className="my-1">
-                {tag}
-              </Tag>
-            );
-          })}
+        {/* Title and Tags Group */}
+        <div>
+          <Title level={3}>{flashcard.title}</Title>
+          <div className="pb-5">
+            {flashcard.tags.map((tag) => {
+              return (
+                <Tag color={getColor(tag)} key={tag} className="my-1">
+                  {tag}
+                </Tag>
+              );
+            })}
+          </div>
         </div>
 
+        {/* Flashcard Content Group */}
         <div className="pb-5">
           <Collapse defaultActiveKey={["question"]}>
             {flashcard.question !== "" && (
@@ -137,6 +194,7 @@ const Flashcard = (props) => {
         </div>
       </div>
 
+      {/* Pagination */}
       {cardList && (
         <div className="py-4 flex-grow-1 d-flex align-items-end">
           <div>
@@ -146,7 +204,6 @@ const Flashcard = (props) => {
               defaultPageSize={1}
               responsive={true}
               onChange={(index) => {
-                console.log(`/flashcard/${cardList[index - 1]}`);
                 history.push({
                   pathname: `/flashcard/${cardList[index - 1]}`,
                   cardList: cardList,
